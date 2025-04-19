e103c2943f69cc9cade4fefada5b0a8155cc9c82
Juliano <72839366+julianocgc@users.noreply.github.com>
2025-04-19 19:59:05 -0300

    Envio de códigos
    Envio de códigos Angular.

--- angular-basico/src/app/app.component.ts
+++ angular-basico/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Componente01Component } from "./componente01/componente01.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Componente01Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ angular-basico/src/app/componente01/componente01.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-componente01',
+  imports: [],
+  templateUrl: './componente01.component.html',
+  styleUrl: './componente01.component.css'
+})
+export class Componente01Component {
+
+}
